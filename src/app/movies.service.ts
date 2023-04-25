4b988419851eda8b9bac5c04ae09ea1e65325f7f
Kushal Mehta <kushal.autotech@gmail.com>
2023-04-25 22:08:00 +0530

    comit

--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -7,26 +7,27 @@ import { Observable } from 'rxjs';
 })
 export class MoviesService {
 
-  constructor(private httpClient:HttpClient) { }
+  url = "https://netizens-backend.onrender.com/api/v1/";
+  constructor(private httpClient: HttpClient) { }
 
-  createMovies(payload:any): Observable<any>{
-    return this.httpClient.post("http://localhost:3000/api/v1/" + 'movie', payload)
-   }
- 
-   deleteMovies(id:any): Observable<any>{
-     return this.httpClient.delete("http://localhost:3000/api/v1/movie/" + id)
-   }
- 
-   getAllMovies(): Observable<any>{
-     return this.httpClient.get("http://localhost:3000/api/v1/movie")
-   }
-
-   getMovieById(id:any): Observable<any>{
-    return this.httpClient.get("http://localhost:3000/api/v1/movie/" + id)
+  createMovies(payload: any): Observable<any> {
+    return this.httpClient.post(this.url + 'movie', payload)
   }
- 
-   editMovieData(id:any,payload:any):Observable<any>{
-     return this.httpClient.put("http://localhost:3000/api/v1/movie/" +id, payload)
-   }
- 
+
+  deleteMovies(id: any): Observable<any> {
+    return this.httpClient.delete(this.url +  "movie/" + id)
+  }
+
+  getAllMovies(): Observable<any> {
+    return this.httpClient.get(this.url + "movie")
+  }
+
+  getMovieById(id: any): Observable<any> {
+    return this.httpClient.get(this.url + "movie/" + id)
+  }
+
+  editMovieData(id: any, payload: any): Observable<any> {
+    return this.httpClient.put(this.url + "movie/" + id, payload)
+  }
+
 }
